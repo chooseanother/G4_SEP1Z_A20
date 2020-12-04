d9f46082eb4a5fd08e1ccbd4ccc8dc0108b4168d
laurentiumaniliuc <laurentiumaniliuc@gmail.com>
2020-12-04 13:23:46 +0100

    TeamMemberList class done

--- LetsStart/src/Model/TeamMemberList.java
+++ LetsStart/src/Model/TeamMemberList.java
@@ -26,10 +26,36 @@ public class TeamMemberList
     return null;
   }
 
+  public ArrayList<TeamMember> getTeamMembersByRole(Role role)
+  {
+    ArrayList<TeamMember> members=new ArrayList<>();
+    for(TeamMember member:list)
+      if(member.getRole().equals(role))
+        members.add(member);
+    return members;
+  }
   public TeamMember getTeamMember(Role role)
   {
-
+    for(TeamMember member:list)
+      if(member.getRole().equals(role))
+        return member;
+    return null;
   }
-
+  public void removeTeamMember(String ID){
+    for(TeamMember member:list)
+      if(member.getID().equals(ID))
+        list.remove(member);
+  }
+  public void removeTeamMember(int index)
+  {
+     list.remove(index);
+  }
+  @Override public String toString()
+  {
+    String s="";
+    for(TeamMember member:list)
+      s=s+" "+member.toString()+" // ";
+    return s;
+  }
 
 }
