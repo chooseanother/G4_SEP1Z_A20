dd407f5d2d9d8d68f95b7d4a2e65a89c77ece642
Kim Tranberg <kim.tranberg@gmail.com>
2020-12-08 15:35:49 +0100

    Test

--- LetsStart/src/Model/TeamMember.java
+++ LetsStart/src/Model/TeamMember.java
@@ -2,33 +2,25 @@ package Model;
 
 public class TeamMember extends Person
 {
-  private String ID;
   private Role role;
   public TeamMember(Name name, String id, String role)
   {
     super(name);
-    ID=id;
     this.role.setRole(role);
   }
-  public void setID(String ID)
-  {
-    this.ID=ID;
-  }
+
   public Role getRole()
   {
     return role;
-  }
-
-  public String getID(){
-    return ID;
   }
 
   public void assignRole(Role role)
   {
     this.role=role;
   }
+
   @Override public String toString()
   {
-    return super.toString()+" "+ID+" "+role.toString();
+    return super.toString()+" "+role.toString();
   }
 }
