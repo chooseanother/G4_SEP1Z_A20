98f3ea61a67727c190c324c0fdea3835a6f61bde
henrikkoster <henrikmiki@gmail.com>
2020-12-08 14:41:52 +0000

    Updated New Requirement View

--- LetsStart/src/View/NewRequirementViewController.java
+++ LetsStart/src/View/NewRequirementViewController.java
@@ -3,10 +3,6 @@ package View;
 import Model.ManagementSystemModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 
