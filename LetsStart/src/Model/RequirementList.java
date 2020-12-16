36b731cd413b9fc8a314a1517bae2cd93243af71
Kim Tranberg <kim.tranberg@gmail.com>
2020-12-16 17:44:28 +0100

    Added check for legal estimated time. (don't do this in documentation branch duh)

--- LetsStart/src/Model/RequirementList.java
+++ LetsStart/src/Model/RequirementList.java
@@ -2,20 +2,37 @@ package Model;
 
 import java.util.ArrayList;
 
+/**
+ * Handles an {@link ArrayList} of {@link Requirement}
+ *
+ * @author Kim
+ */
 public class RequirementList
 {
   private ArrayList<Requirement> list;
 
+  /**
+   * Initializes an array list
+   */
   public RequirementList()
   {
     list = new ArrayList<>();
   }
 
+  /**
+   * Adds specified requirement to the list
+   * @param requirement as an {@link Requirement}
+   */
   public void addRequirement(Requirement requirement)
   {
     list.add(requirement);
   }
 
+  /**
+   * Returns a requirement with specified id
+   * @param ID as an {@link Integer}
+   * @return a {@link Requirement} <p>If not found it will return null</p>
+   */
   public Requirement getRequirementId(int ID)
   {
     for(Requirement requirement:list)
@@ -25,16 +42,32 @@ public class RequirementList
       }
     return null;
   }
+
+  /**
+   * Returns a requirement at specified index in the list
+   *
+   * @param index as an {@link Integer}
+   * @return a {@link Requirement}
+   */
   public Requirement getRequirementIndex(int index)
   {
     return list.get(index);
   }
 
+  /**
+   * Returns number of requirements currently in the list
+   * @return amount as an {@link Integer}
+   */
   public int getNumberOfRequirements()
   {
     return list.size();
   }
 
+  /**
+   * Gets all requirements based on status of linked parameter
+   * @param status as a {@link String}
+   * @return an {@link ArrayList} of type {@link Requirement}
+   */
   public ArrayList<Requirement> getRequirementByStatus(String status){
     ArrayList<Requirement> reqByStatus = new ArrayList<>();
     for (Requirement req : list){
@@ -45,25 +78,32 @@ public class RequirementList
     return reqByStatus;
   }
 
+  /**
+   * Removes a requirement with specified id from the list
+   * @param id as an {@link Integer}
+   */
   public void removeRequirement(int id)
   {
     list.removeIf(req -> req.getId() == id);
   }
 
+  /**
+   * Removes specified requirement from the list
+   * @param requirement as an {@link Requirement}
+   */
   public void removeRequirement(Requirement requirement)
   {
     list.remove(requirement);
   }
 
+  /**
+   * Creates a string with information from all requirements currently in a list
+   * @return a {@link Requirement}
+   */
   @Override public String toString()
   {
-    StringBuilder s= new StringBuilder();
-    for(Requirement requirement:list)
-    {
-      s.append(requirement.toString());
-    }
+    StringBuilder s= new StringBuilder("Requirement list: ");
+    list.forEach(r -> s.append("\n").append(r));
     return s.toString();
   }
-
-
 }
